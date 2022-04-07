2ee35258512580691fe90c52f9011f8b13f6f73d
cros <cros@nextads.es>
2022-04-07 09:39:03 +0200

    [FIX] Revisar dominio

--- project_custom/models/project_project.py
+++ project_custom/models/project_project.py
@@ -5,10 +5,10 @@ from odoo import models, fields, api, _
 class Project(models.Model):
     _inherit = "project.project"
 
-    @api.onchange('partner')
+    @api.onchange('partner_id')
     def onchange_partner_id(self):
         for project in self:
-            return {'domain':{'responsible_customer' [('id','in',project.partner.child_ids.ids)]}}
+            return {'domain': {'responsible_customer': [('parent_id', '=', project.partner_id.id)]}}
 
     init_date = fields.Date(string="Init Date", required=False,)
     end_date = fields.Date(string="End Date", required=False,)
