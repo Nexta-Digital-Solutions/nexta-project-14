05e0cf0bbe55bbfc9591adf1b1621ff69e6a5c19
cros <cros@nextads.es>
2022-04-26 17:19:47 +0200

    [ADD] Campos fecha de proyectos

--- project_custom/models/project_timesheet_type_line.py
+++ project_custom/models/project_timesheet_type_line.py
@@ -6,12 +6,22 @@ class ProjectTimesheetTypeLine(models.Model):
     _name = "project.timesheet.type.line"
     _description = "Project Timesheet types lines"
 
+    name = fields.Char(string="Name", computed="compute_name", store=True)
     timesheet_type = fields.Many2one(comodel_name="project.timesheet.type", string="Type", required=True)
     total_hours = fields.Float(string="Total Hours", required=False,)
     hours_spent = fields.Float(string="Hours Spent", required=False, compute="_compute_spent_hours", store=True)
     remaining_hours = fields.Float(string="Remaining Hours", required=False, compute="_compute_spent_hours", store=True)
     spend_percentage = fields.Float(string="Remaining percent", required=False, compute="_compute_spent_hours", store=True)
-    project_id = fields.Many2one(comodel_name="project.project", string="Project",)
+    project_id = fields.Many2one(comodel_name="project.project", string="Project",required=True)
+    project_init_date = fields.Date(string="Project Init Date", related="project_id.init_date")
+    project_end_date = fields.Date(string="Project End Date", related="project_id.end_date")
+
+
+    @api.onchange('timesheet_type', 'project_id')
+    def compute_name(self):
+        for line in self:
+            if line.timesheet_type and line.project_id:
+                line.name = "[{}]{}".format(line.project_id.name, line.timesheet_type.name)
 
     @api.onchange('project_id.task_ids.timesheet_ids', 'hours_spent')
     @api.depends('project_id.task_ids.timesheet_ids','hours_spent')
