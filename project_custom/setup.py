a120d60c15dad4a8ce879178d32a3c9e3b9773a0
cros <cros@nextads.es>
2023-08-22 09:29:17 +0200

    [ADD] Script instalar libreria

---
+++ project_custom/setup.py
@@ -0,0 +1,15 @@
+class Packages:
+    """
+    This Class installs required Packages or library
+    """
+
+    get_pckg = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in get_pckg.split()]
+    # List of your required packages
+    required_packages = ['openupgradelib']
+    for packg in required_packages:
+        if packg in installed_packages:
+            pass
+        else:
+            print('installing package %s' % packg)
+            os.system('pip3 install ' + packg)
